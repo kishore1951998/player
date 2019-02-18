8feed7a737586907eb0b9bf70d3bf42d73049dc1
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-18 13:37:26 +0530

    Create 13.c

---
+++ 13.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main(void) 
+{
+	int num,sum=0,sq,rem;
+  clrscr();
+	scanf("%d",&num);
+	while(num!=0)
+	{
+		rem=num%10;
+		sq=rem*rem;
+		sum=sum+sq;
+		num=num/10;
+	}
+	printf("\n%d",sum);
+	return 0;
+}
+ 
