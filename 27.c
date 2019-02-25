abac377e5e8f5fee8cdb5a4719a13ad33a0efa67
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-25 07:53:43 +0530

    Create 27.c

---
+++ 27.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+char a[100];
+int i;
+clrscr();
+scanf("%[^\n]",a);
+for (i=0;a[i]!='\0';i++)
+{
+if(a[i]>=65&&a[i]<=90)
+{
+a[i]=a[i]+32;
+}
+else if( a[i]>=97&&a[i]<=122)
+{
+a[i]=a[i]-32;
+}
+printf("%c",a[i]);
+}
+}
