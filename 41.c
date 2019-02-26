79fdf644555edf35dd5b35e79fc2ec962ec649d2
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-26 07:55:24 +0530

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() 
+{
+   int n,k,i,m=1,d=0;
+   clrscr();
+   scanf("%d%d",&n,&k);
+   for(i=2;i<n;i++)
+   {
+       m=m*k;
+       if(m==n)
+       {
+           d++;
+           break;
+       }}
+       if(d!=0)
+       printf("yes");
+       else
+       printf("no");
+   return 0;
+}
