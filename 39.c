39b7c45f418f0666d34937db0a4fa56f0ca4811d
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-26 07:51:15 +0530

    Create 39.c

---
+++ 39.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int a,k=2,i,l=0;
+    clrscr();
+    scanf("%d",&a);
+    k=2;
+    for(i=0;i<a;i++)
+    {
+        k=k*2;
+        if(k==a)
+        {
+            l++;
+            break;
+        }
+    }
+    if((l!=0)||(a==1))
+    printf("yes");
+    else if(l==0)
+    printf("no");
+    return 0;
+}
