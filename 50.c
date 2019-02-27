93fd55645dbe43bc55fdeba6fbe9d303a1e75cb0
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-27 07:59:59 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    int n,i,f=0;
+    clrscr();
+    scanf("%d",&n);
+    for(i=2;i<n/2;i++)
+    {
+        if(n%i==0)
+        {
+            f=1;
+        }
+    }
+    if(f==1)
+    printf("yes");
+    else
+    printf("no");
+    getch();
+}
