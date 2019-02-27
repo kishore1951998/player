2727aade083b2f7c83adfa6656c3e6177c95f1af
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-27 15:41:39 +0530

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    char a[100],c;
+    int i,l=0;
+    printf("Enter string and character");
+    scanf("%s %c",a,&c);
+    for(i=0;a[i]!='\0';i++)
+    {
+        if(a[i]==c)
+        {
+            printf("%d",i+1);
+            break;
+        }
+        
+    }
+    return 0;
+}
