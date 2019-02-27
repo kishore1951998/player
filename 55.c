41a432712557d638e33e3ddd86e4945df739a500
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-02-27 15:35:26 +0530

    Create 55.c

---
+++ 55.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+   char a[100],b[100];
+   int i,l=0,m=0,n=0,j;
+   printf("Enter a and b : ");
+   scanf("%s",a);
+   scanf("%s",b);
+   for(i=0;a[i]!='\0';i++)
+   {
+       l++;
+   }
+       for(j=0;b[j]!='\0';j++)
+       {
+           m++;
+           
+       }
+           if(m==l)
+           {
+           for(i=0;i<l;i++)
+           {
+           if((a[i]==b[i])||(a[i]==b[i]+32)||(a[i]==b[i]-32))
+                  {
+                      n++;
+                  }
+               
+           }
+               
+           }
+   if(n==m)
+   {
+       printf("Yes");
+   }
+   else
+   printf("No");
+   return 0;
+ 
+}
