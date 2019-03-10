62ea8d7c9f9b334975374da82fe2d62a00edeafa
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-03-10 17:37:39 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int n,k,a[100],i;
+printf("Enter the value");
+scanf("%d %d",&n,&k);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+if(a[i]==k)
+{
+printf("%d",i+1);
+}
+}
+getch();
+}
