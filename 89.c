4bab6fcb57c931f4516ca3e91bd1bfccf00b7ebd
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-03-10 18:52:56 +0530

    Create 89.c

---
+++ 89.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int n,k,res=1,i;
+  printf("Enter the value");
+	scanf("%d %d",&n,&k);
+	if(n>=k)
+	{
+	for(i=0;i<k;i++)
+	{
+	    res=res*n;
+	    n--;
+	}
+	printf("%d",res);
+	}
+	else
+	{
+	    printf("not valid input");
+	}
+	return 0;
+}
