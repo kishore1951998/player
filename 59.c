775f01bb7a6bc32a542c0cdc1ec5c2c0eab20e10
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-03-10 16:29:44 +0530

    Create 59.c

---
+++ 59.c
@@ -0,0 +1,25 @@
+    
+#include<stdio.h>
+
+int main() {
+   char a[100];
+   int i,j,l;
+   printf("Enter the value");
+   scanf("%d",&l);
+   scanf("%s",a);
+   for(i=0;i<l;i++)
+   {
+       for(j=i+1;j<l;j++)
+       {
+           if(a[i]=='1')
+           {
+               if(a[j]=='0')
+               {
+                   printf("1");
+                   break;
+               }
+           }
+       }
+   }
+return 0;
+}
