aecb40534333c5f8acac4ca6cdea9cd30e3a2f72
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-03-10 18:19:14 +0530

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    long int n,a[100],i=0,k,p,t,s,c=1,rem;
+    printf("Enter the value");
+    scanf("%ld %ld %ld",&n,&k,&p);
+    t=n;
+    while(t>0)
+    {
+        t=t/10;
+        c=c*10;
+    }
+    c=c/10;
+    while(n>0)
+    {
+        rem=n/c;
+        a[i]=rem;
+        i++;
+        n=n%c;
+        c=c/10;
+    }
+    while(k>0)
+    {
+        s=a[p];
+        k--;
+        p++;
+    }
+    printf("%ld ",s);
+    return 0;
+}
