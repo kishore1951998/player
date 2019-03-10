053a098324f40079b32ea25e7f711a8d3772e63c
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-03-10 17:34:48 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+  int n,k=1,i,a[100],j;
+  printf("Enter the value");
+  scanf("%d",&j);
+   scanf("%d",&n);
+  for(i=0;i<j;i++)
+  {
+      k=k*n;
+  }
+  printf("%d",k);
+  return 0;
+}
