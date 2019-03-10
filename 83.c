e162ab8946b3cbd57aaf663f7b38988359bf7e41
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-03-10 18:42:52 +0530

    Create 83.c

---
+++ 83.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int N,i,j,max,f;
+    printf("Enter the value");
+    scanf("%d",&N);
+    int a[N];
+    for(i=0;i<N;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<N;i++)
+	{
+		for(j=i+1;j<N;j++)
+		{
+			f=((a[i]) | (a[j]));
+		}
+	}
+	printf("%d",f);
+    return 0;
+}
