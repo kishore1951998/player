fcd731929320c99c339f2a9574c077ae3b61b076
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-03-10 16:48:02 +0530

    Create 70.c

---
+++ 70.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main() {
+   char a[100],i,b[100]={'A','n','s','w','e','r'};
+   printf("Enter the value");
+   scanf("%s",a);
+   printf("%s\t",a);
+    printf("%s",b);
+    return 0;
+}
