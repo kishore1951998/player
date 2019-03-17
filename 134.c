4afec098ec02f6ad5a57c070e0fc829d96507c01
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-03-17 19:06:48 +0530

    Create 134.c

---
+++ 134.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int n,l,r,m,a[100],k,i;
+printf("Enter the value");
+scanf("%d %d %d",&n,&l,&r);
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+m=l-1;
+k=a[m];
+for(i=l-1;i<k;i++)
+{
+if(a[i]<k)
+{
+k=a[i];
+}
+}
+printf("%d",k);
+}
