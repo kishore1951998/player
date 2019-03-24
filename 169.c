e46101d9cc60e980d8d4e0a8ce0eafafd4ffb794
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-03-24 13:53:41 +0530

    Create 169.c

---
+++ 169.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+char a[100],i,j,count;
+printf("Enter the  a value");
+scanf("%s",a);
+for(i=0;a[i]!='\0';i++)
+{
+count=0;
+if(a[i]!='$')
+{
+for(j=i+1;a[j]!='\0';j++)
+{
+if(a[i]==a[j])
+{
+count++;
+a[j]='$';
+}
+}
+printf("%c%d",a[i],count+1);
+}
+}
+return 0;
+}
