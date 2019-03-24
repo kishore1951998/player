dbafa8279c7b456a7e7f7d6056b76da8d230de7e
kishore1951998 <46838611+kishore1951998@users.noreply.github.com>
2019-03-24 12:13:41 +0530

    Create 151.c

---
+++ 151.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<string.h>
+int main() 
+{
+   char a[100];
+   int i,l=0,c=0;
+   printf("Enter the value");
+   scanf("%s",a);
+   l=strlen(a);
+   for(i=0;i<l;i++)
+   {
+       if((a[i]!='a')&&(a[i]!='b'))
+       {
+           c++;
+           break;
+       }
+   }
+   if(c!=0)
+   printf("no");
+   else
+   printf("yes");
+   return 0;
+}
